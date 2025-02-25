0d3ba1542c76445e40aaa837e56556e5ef17c84a
Marat Shakirov <marat.s@affinidi.com>
2025-02-25 23:25:20 +0000

    feat: update all clients

--- src/Clients/LoginConfigurationClient/test/Model/TokenEndpointAuthMethodTest.php
+++ src/Clients/LoginConfigurationClient/test/Model/TokenEndpointAuthMethodTest.php
@@ -5,7 +5,7 @@
  * PHP version 7.4
  *
  * @category Class
- * @package  OpenAPI\Client
+ * @package  AffinidiTdk\Clients\LoginConfigurationClient
  * @author   OpenAPI Generator team
  * @link     https://openapi-generator.tech
  */
@@ -27,7 +27,7 @@
  * Please update the test case below to test the model.
  */
 
-namespace OpenAPI\Client\Test\Model;
+namespace AffinidiTdk\Clients\LoginConfigurationClient\Test\Model;
 
 use PHPUnit\Framework\TestCase;
 
@@ -36,7 +36,7 @@ use PHPUnit\Framework\TestCase;
  *
  * @category    Class
  * @description Requested Client Authentication method for the Token Endpoint. The options are: &#x60;client_secret_post&#x60;: (default) Send client_id and client_secret as application/x-www-form-urlencoded in the HTTP body. &#x60;client_secret_basic&#x60;: Send client_id and client_secret as application/x-www-form-urlencoded encoded in the HTTP Authorization header. &#x60;none&#x60;: For public clients (native/mobile apps) which can not have secret.
- * @package     OpenAPI\Client
+ * @package     AffinidiTdk\Clients\LoginConfigurationClient
  * @author      OpenAPI Generator team
  * @link        https://openapi-generator.tech
  */
---
+++ src/Clients/VaultDataManagerClient/docs/Model/ListNodeChildrenOK.md
@@ -0,0 +1,10 @@
+# # ListNodeChildrenOK
+
+## Properties
+
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**nodes** | [**\AffinidiTdk\Clients\VaultDataManagerClient\Model\NodeDto[]**](NodeDto.md) |  | [optional]
+**last_evaluated_key** | **string** |  | [optional]
+
+[[Back to Model list]](../../README.md#models) [[Back to API list]](../../README.md#endpoints) [[Back to README]](../../README.md)
