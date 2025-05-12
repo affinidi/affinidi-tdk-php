33bc263c3c98dea5158220150625d49f3115deb1
Mohamed Abdelsamei <mohamed.e@affinidi.com>
2025-05-12 13:24:26 +0200

    feat: add cis issuance integration test (#177)

--- tests/Integration/CredentialIssuanceClientIntegrationTest.php
+++ tests/Integration/CredentialIssuanceClientIntegrationTest.php
@@ -31,7 +31,7 @@ class CredentialIssuanceClientIntegrationTest extends TestCase
 
     public function testIssuanceConfig()
     {
-        $config = CredentialIssuanceClient\Configuration::getDefaultConfiguration()->setApiKey('authorization', '', getTokenCallback());
+        $config = CredentialIssuanceClient\Configuration::getDefaultConfiguration()->setApiKey('Authorization', '', getTokenCallback());
 
         $api = new CredentialIssuanceClient\Api\ConfigurationApi(
             new GuzzleHttp\Client(),
@@ -43,8 +43,8 @@ class CredentialIssuanceClientIntegrationTest extends TestCase
 
         debugMessage('List Credential Issuance Configurations', ['result' => $resultJson]);
 
-        // If we have issuance configuration, gets its ID, otherwise create a new one
-        if (count($resultJson) == 1) {
+        // Check if there are any configurations in the list
+        if (!empty($resultJson['configurations'])) {
             $configId = $resultJson['configurations'][0]['id'];
 
             // Assert that 'id' key exists
@@ -97,5 +97,116 @@ class CredentialIssuanceClientIntegrationTest extends TestCase
 
         $this->assertEquals($resultJson['webhook']['enabled'], true);
         $this->assertArrayHasKey('url', $resultJson['webhook']['endpoint'], 'The response does not contain a "url" key.');
+    }
+
+
+    private function batchCredentialStartIssuance(): array
+    {
+        $config = CredentialIssuanceClient\Configuration::getDefaultConfiguration()->setApiKey('authorization', '', getTokenCallback());
+
+
+        $api = new CredentialIssuanceClient\Api\IssuanceApi(
+            new GuzzleHttp\Client(),
+            $config
+        );
+        $projectId = getConfiguration()['projectId'];
+        $issuanceData = getConfiguration()['issuanceData'];
+
+        $result = $api->startissuance($projectId, json_decode($issuanceData));
+        $resultJson = json_decode($result, true);
+        debugMessage('Start Credential Issuance Response', ['result' => $resultJson], true);
+        return $resultJson;
+    }
+    // function to getOfferUri and return preAuthCode
+    private function batchCredentialGetOfferUri($issuanceId): array
+    {
+        $config = CredentialIssuanceClient\Configuration::getDefaultConfiguration();
+
+        $api = new CredentialIssuanceClient\Api\OfferApi(
+            new GuzzleHttp\Client(),
+            $config
+        );
+        $projectId = getConfiguration()['projectId'];
+
+        $result = $api->getCredentialOffer($projectId, $issuanceId);
+        $resultJson = json_decode($result, true);
+
+        debugMessage('Get Offer Uri Response', ['result' => $result], true);
+
+        // Assert that 'preAuthCode' key exists
+        $this->assertArrayHasKey('credential_issuer', $resultJson, 'The response does not contain a "preAuthCode" key.');
+        $this->assertArrayHasKey('grants', $resultJson, 'The response does not contain a "preAuthCode" key.');
+        $this->assertArrayHasKey('urn:ietf:params:oauth:grant-type:pre-authorized_code', $resultJson['grants'], 'The response does not contain a "preAuthCode" key.');
+        $this->assertArrayHasKey('pre-authorized_code', $resultJson['grants']['urn:ietf:params:oauth:grant-type:pre-authorized_code'], 'The response does not contain a "preAuthCode" key.');
+        $preAuthCode = $resultJson['grants']['urn:ietf:params:oauth:grant-type:pre-authorized_code']['pre-authorized_code'];
+        $credentialIssuer = $resultJson['credential_issuer'];
+        return [
+            'preAuthCode' => $preAuthCode,
+            'credentialIssuer' => $credentialIssuer
+        ];
+    }
+
+
+
+    private function batchCredentialExchangeToken($preAuthCode, $credentialIssuer): array
+    {
+        $url = "{$credentialIssuer}/oauth2/token";
+        $requestBody = http_build_query([
+            'grant_type' => 'urn:ietf:params:oauth:grant-type:pre-authorized_code',
+            'pre-authorized_code' => $preAuthCode,
+        ]);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $requestBody);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/x-www-form-urlencoded'
+        ]);
+
+        $response = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        if ($httpCode !== 200) {
+            throw new \RuntimeException("Token exchange failed with status code: {$httpCode}");
+        }
+
+        $result = json_decode($response, true);
+        return [
+            'access_token' => $result['access_token'],
+            'authorization_details' => $result['authorization_details'],
+        ];
+    }
+
+    public function testBatchIssuance()
+    {
+        debugMessage('Start Batch Credential Issuance', ['projectId' => getConfiguration()['projectId'], 'issuanceData' => getConfiguration()['issuanceData']]);
+        $startIssuanceResponse = $this->batchCredentialStartIssuance();
+
+        // Assert start issuance response
+        $this->assertArrayHasKey('issuanceId', $startIssuanceResponse, 'The response does not contain an "issuanceId" key.');
+        $this->assertArrayHasKey('credentialOfferUri', $startIssuanceResponse, 'The response does not contain a "credentialOfferUri" key.');
+        $this->assertNotEmpty($startIssuanceResponse['issuanceId'], 'The issuanceId should not be empty.');
+
+        $issuanceId = $startIssuanceResponse['issuanceId'];
+        $offerInfo = $this->batchCredentialGetOfferUri($issuanceId);
+
+        // Assert offer info response
+        $this->assertArrayHasKey('preAuthCode', $offerInfo, 'The response does not contain a "preAuthCode" key.');
+        $this->assertArrayHasKey('credentialIssuer', $offerInfo, 'The response does not contain a "credentialIssuer" key.');
+        $this->assertNotEmpty($offerInfo['preAuthCode'], 'The preAuthCode should not be empty.');
+        $this->assertNotEmpty($offerInfo['credentialIssuer'], 'The credentialIssuer should not be empty.');
+
+        $preAuthCode = $offerInfo['preAuthCode'];
+        $credentialIssuer = $offerInfo['credentialIssuer'];
+        $tokenInfo = $this->batchCredentialExchangeToken($preAuthCode,$credentialIssuer);
+
+        // Assert token info response
+        $this->assertArrayHasKey('access_token', $tokenInfo, 'The response does not contain a "access_token" key.');
+        $this->assertArrayHasKey('authorization_details', $tokenInfo, 'The response does not contain a "authorization_details" key.');
+        $this->assertNotEmpty($tokenInfo['access_token'], 'The access_token should not be empty.');
+        $this->assertNotEmpty($tokenInfo['authorization_details'], 'The authorization_details should not be empty.');
     }
 }
